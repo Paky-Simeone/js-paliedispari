971abc5433f25b43b39e53d84011602d1c96655a
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-01-17 18:17:05 +0100

    first commit

---
+++ Pari e Dispari/js/main.js
@@ -0,0 +1,48 @@
+// Dichiaro constanti e stampo in console
+const pariodispari = prompt('scegli pari o dispari');
+console.log(pariodispari);
+const numeroUtente = parseInt(prompt('Scegli un numero da 1 a 5'));
+console.log("il numero dell'utente è: " + numeroUtente);
+
+// Controllo se il numero utente è effettivamente un numero o meno
+if(isNaN(numeroUtente)){
+    alert('il valore inserito non è un numero, riprova.');
+}
+
+// Creo funzione per generare un numero random
+function getRandomNumber(min, max){
+    const numeroRandom = Math.floor(Math.random() * (max - min +1) + min);
+    return numeroRandom;
+}
+
+// Dichiaro e stampo in console il numero random della cpu 
+const numerocpu = getRandomNumber(1, 5);
+console.log('il numero della cpu è: ' + numerocpu);
+
+// Faccio la somma del numero utente più il numero della cpu
+let numerofinale = numeroUtente + numerocpu;
+console.log('la somma di utente e cpu è: ' + numerofinale);
+
+// Creo una funzione che controlla se il numero è pari o dispari
+function getParioDispari(numero){
+    if(numero % 2 == 0){
+        // alert('la somma è pari');
+        console.log('il numero finale è pari');
+    }else{
+        // alert('la somma è dispari');
+        console.log('il numero finale è dispari');
+    }
+
+    return numero % 2 == 0;
+}
+
+console.log(getParioDispari(numerofinale));
+
+// Stampo il vincitore
+if(pariodispari == pari){
+    alert('utente vincitore');
+}else {
+    alert('pc vincitore');
+}
+
+
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Traccia 1
+1. Palidroma
+- Chiedere all'utente di inserire una parola
+- Creare una funzione per capire se la parola inserita è palindroma
+# Traccia 2
+2. Pari e Dispari
+- L'utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+- Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+- Sommiamo i due numeri
+- Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+- Dichiariamo chi ha vinto.
+
+## Svolgimento Traccia 1
+
+## Svolgimento Traccia 2
+- Chiediamo all'utente di scegliere se pari o dispari.
+- Chiediamo all'utente di inserire un numero da 1 a 5.
+- Generiamo per il pc un numero random da 1 a 5 tramite una funzione
+- Sommiamo il numero dell'utente con il numero del pc
+- Tramite una funzione controlliamo se la somma dei due numeri è pari o dispari
+- Stampiamo il vincitore.
